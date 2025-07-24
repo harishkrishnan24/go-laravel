f724146ba67a5990550e26ebf8ab02e39a01e728
harishkrishnan24 <harishkrishnan1993@gmail.com>
2025-07-24 14:39:03 +0200

    add home route using jet template

--- myapp/routes.go
+++ myapp/routes.go
@@ -8,10 +8,9 @@ import (
 
 func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/", a.Handlers.Home)
-	a.App.Routes.Get("/jet", func(w http.ResponseWriter, r *http.Request) {
-		a.App.Render.JetPage(w, r, "testjet", nil, nil)
-	})
+
 	fileServer := http.FileServer(http.Dir("./public"))
 	a.App.Routes.Handle("/public/*", http.StripPrefix("/public", fileServer))
+
 	return a.App.Routes
 }
